b79cfcd804b442bf1303e025698f99dd2fe9243c
r.g.s <anx99@rgs.com>
2019-11-04 19:47:51 +0100

    Simple conversion routines

--- src/utils/conversion.rs
+++ src/utils/conversion.rs
@@ -1,9 +1,16 @@
 use crate::ff::{PrimeField, QuadraticExtension};
 
+use std::str::FromStr;
+
 pub fn str_to_u64(s: &str) -> u64 {
-    unimplemented!()
+    u64::from_str_radix(s, 16).expect(
+        &format!("Incorrect value: {}", s)
+    )
 }
 
 pub fn str_to_p434(s0: &str, s1: &str) -> QuadraticExtension<PrimeField> {
-    unimplemented!()
+    QuadraticExtension::from(
+        PrimeField::from_string(s0),
+        PrimeField::from_string(s1)
+    )
 }
