b5eeb57fabf1538980e75ab8201872f075c45b4a
Ashashin <edmond.de-roffignac@student.ecp.fr>
2019-11-04 20:37:01 +0100

    implem p434 finite field

--- src/utils/conversion.rs
+++ src/utils/conversion.rs
@@ -1,9 +1,14 @@
-use crate::ff::{PrimeField, QuadraticExtension};
+use num_bigint::BigInt;
+use crate::ff::{PrimeField_p434, QuadraticExtension};
 
 pub fn str_to_u64(s: &str) -> u64 {
     unimplemented!()
 }
 
-pub fn str_to_p434(s0: &str, s1: &str) -> QuadraticExtension<PrimeField> {
+pub fn str_to_p434(s0: &str, s1: &str) -> QuadraticExtension<PrimeField_p434> {
+    unimplemented!()
+}
+
+pub fn str_to_bigint(s: &str) -> BigInt {
     unimplemented!()
 }
